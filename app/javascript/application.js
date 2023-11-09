309d84a93562183a36755896f31a01a280c8f523
I am Duy <bduy2015@gmail.com>
2023-11-09 19:15:22 +0700

    Fixed dupcode

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,127 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "./common.js"
+
+$(document).on('click', '.un-like-product', function(e) {
+  const url = $(this).data('url')
+  const productId = $(this).data('product-id')
+  console.log('unlike')
+  $.ajax({
+    url: url,
+    type: 'POST',
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    },
+    data: {
+      product_id: productId
+    },
+    success: function(res) {
+      // console.log(res)
+    }
+  })
+})
+
+
+$(document).on('click', '.like-product', function(e) {
+  const productId = $(this).data('product-id')
+  const url = $(this).data('url') + `/${productId}`
+  console.log('like')
+  $.ajax({
+    url: url,
+    type: 'DELETE',
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    },
+    data: {
+      product_id: productId
+    },
+    success: function(res) {
+      // console.log(res)
+    }
+  })
+})
+
+
+$(document).on('click', '.add-to-cart', function(e) {
+  const url = $(this).data('url')
+  const productId = $(this).data('product-id')
+
+  console.log('bbbb')
+
+  $.ajax({
+    url: url,
+    type: 'POST',
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    },
+    data: {
+      product_id: productId,
+    },
+    success: function(res) {
+      // console.log(res)
+    }
+  })
+})
+
+
+
+$(document).on('click', '.plus-cart-item', function(e) {
+  const url = $(this).data('url')
+  const productId = $(this).data('product-id')
+  const operation = $(this).data('operation')
+  const cartitemquantity = $(this).data('cart-item-quantity')
+  const productquantity = $(this).data('product-quantity')  
+  console.log(cartitemquantity)
+  console.log(productquantity)
+
+  if ((cartitemquantity + 1) > productquantity) {
+    console.log('unlike')
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: `Available quantity of this product is ${productquantity}`,
+      footer: '<a href="#">Why do I have this issue?</a>'
+    });
+  }
+
+  
+  $.ajax({
+    url: url,
+    type: 'POST',
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    },
+    data: {
+      product_id: productId,
+      operation: operation,
+    },
+    success: function(res) {
+      // console.log(res)
+    }
+  })
+})
+
+
+$(document).on('click', '.minus-cart-item', function(e) {
+  const url = $(this).data('url')
+  const productId = $(this).data('product-id')
+  const operation = $(this).data('operation')
+  console.log('like')
+  
+  $.ajax({
+    url: url,
+    type: 'POST',
+    headers: {
+      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+    },
+    data: {
+      product_id: productId,
+      operation: operation,
+    },
+    success: function(res) {
+      // console.log(res)
+    }
+  })
+})
+
+
--- app/javascript/controllers/change_quantity_icon_cart_controller.js
+++ app/javascript/controllers/change_quantity_icon_cart_controller.js
@@ -1,31 +1,31 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="change-quantity-icon-cart"
-export default class extends Controller {
-  connect() {
-    this.changeQuantityIconCart()
-    console.log('a')
-  }
-  changeQuantityIconCart () {
-    $(document).on('click', '.add-to-cart', function(e) {
-      const url = $(this).data('url')
-      const productId = $(this).data('product-id')
+// // Connects to data-controller="change-quantity-icon-cart"
+// export default class extends Controller {
+//   connect() {
+//     this.changeQuantityIconCart()
+//     console.log('a')
+//   }
+//   changeQuantityIconCart () {
+//     $(document).on('click', '.add-to-cart', function(e) {
+//       const url = $(this).data('url')
+//       const productId = $(this).data('product-id')
 
-      console.log('bbbb')
+//       console.log('bbbb')
 
-      $.ajax({
-        url: url,
-        type: 'POST',
-        headers: {
-          'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-        },
-        data: {
-          product_id: productId,
-        },
-        success: function(res) {
-          // console.log(res)
-        }
-      })
-    })
-  }
-}
+//       $.ajax({
+//         url: url,
+//         type: 'POST',
+//         headers: {
+//           'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+//         },
+//         data: {
+//           product_id: productId,
+//         },
+//         success: function(res) {
+//           // console.log(res)
+//         }
+//       })
+//     })
+//   }
+// }
